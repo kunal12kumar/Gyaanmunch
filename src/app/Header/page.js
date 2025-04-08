0b10bc152189c206fec1ed454061874e1b52c615
kunal12kumar <23cs3035@rgipt.ac.in>
2025-04-08 17:07:53 +0530

    removing bug

--- src/app/Header/page.js
+++ src/app/Header/page.js
@@ -41,7 +41,7 @@ export default function Header() {
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img
+                            <ima
                                 alt="Tailwind CSS Navbar component"
                                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                         </div>
